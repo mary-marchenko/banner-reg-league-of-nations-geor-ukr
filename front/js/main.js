fa612574311e41d3f3d4a26af4dfb86987e5dc48
Mari <mari@MacBook-Pro.local>
2024-11-14 11:54:25 +0200

    padding bug fix

--- front/js/main.js
+++ front/js/main.js
@@ -6,3 +6,30 @@
     setHeight();
     window.addEventListener('resize', setHeight);
 })();
+
+window.addEventListener("orientationchange", () => {
+    location.reload(true)
+});
+
+// color padding-top
+document.addEventListener("DOMContentLoaded", function() {
+    function isIOS() {
+        return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    }
+
+    function isMacOS() {
+        return navigator.platform === 'MacIntel' && !navigator.userAgent.includes('Mobile');
+    }
+
+    let bannerTeam = document.querySelector('.banner__team');
+    let bannerDate = document.querySelector('.banner__date');
+    let bannerTime = document.querySelector('.banner__time');
+    let bannerInfoTop = document.querySelector('.banner__info-top');
+
+    if (!isIOS() && !isMacOS() && itemMagenta && bannerDate && bannerTime) {
+        bannerTeam.classList.add('noios-padding');
+        bannerDate.classList.add('noios-padding');
+        bannerTime.classList.add('noios-padding');
+        bannerInfoTop.classList.add('noios-padding');
+    }
+});
